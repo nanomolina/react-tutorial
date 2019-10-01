199613989cc0d835c68261f5304180ca98f8cdf6
Nano Molina <nano@moni.com.ar>
2019-10-01 19:03:19 -0300

    basic game working

--- src/index.js
+++ src/index.js
@@ -2,33 +2,77 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
+function Square(props) {
     return (
-      <button
-        className="square"
-        onClick={() => this.setState({value: 'X'}) }
-      >
-        {this.state.value}
+      <button className="square" onClick={props.onClick} >
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  getNextMovement() {
+    const nextMovement = this.state.xIsNext ? 'X' : 'O';
+    return nextMovement
+  }
+
+  calculateWinner() {
+    const squares = this.state.squares.slice();
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    const doNothing = this.calculateWinner(squares) || squares[i];
+    if (doNothing) {
+      return;
+    }
+    squares[i] = this.getNextMovement();
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = this.calculateWinner();
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.getNextMovement();
+    }
 
     return (
       <div>
